af2e6ecf26b5ce25d944b768284d23dcfd4f9b1b
Richard Gracik <r.gracik@gmail.com>
2023-12-17 11:04:33 +0000

    sync pythonoviny, rozbite to je cil este

---
+++ cheerlights.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+
+#
+# zapinani cheerlights
+# v sudo nano /etc/rc.local 
+#
+
+
+
+# Import the library functions we need
+from __future__ import division
+import time
+from urllib.request import urlopen
+import serial
+import colorsys
+
+arduino = serial.Serial("/dev/ttyACM0", 9600)
+
+
+# A function to set the LedBorg colours
+def SetLedBorg(red, green, blue):
+    hue, sat, val = colorsys.rgb_to_hsv(red,green,blue)
+    print("Sending hue: " + str(int(hue*255)))
+    arduino.write(str(int(hue*255)).encode())
+
+# A function to turn the LedBorg off
+def LedBorgOff():
+    SetLedBorg(0, 0, 0)
+
+# Setup parameters
+cheerlightsUrl = 'http://api.thingspeak.com/channels/1417/field/1/last.txt'
+#            Name           Red     Green   Blue
+colourMap = {'red':         (1.0,   0.0,    0.0),
+             'green':       (0.0,   1.0,    0.0),
+             'blue':        (0.0,   0.0,    1.0),
+             'cyan':        (0.0,   1.0,    1.0),
+             'white':       (1.0,   1.0,    1.0),
+             'warmwhite':   (1.0,   1.0,    0.9),
+             'purple':      (0.5,   0.0,    0.5),
+             'magenta':     (1.0,   0.0,    1.0),
+             'yellow':      (1.0,   1.0,    0.0),
+             'orange':      (1.0,   0.65,   0.0),
+             'pink':        (1.0,   0.75,   0.8),
+             'oldlace':     (1.0,   1.0,    0.9)}
+
+while True:
+    try:                                                # Attempt the following:
+        with urlopen(cheerlightsUrl) as colourResponse:
+            colourName = colourResponse.read().decode("utf-8")
+            if colourName in colourMap:                   # If we recognise this colour name then ...
+                red, green, blue = colourMap[colourName]            # Get the LedBorg colour to use from the name
+            else:
+                red, green, blue = (0.0, 0.5, 0.5)
+            print("current color: " + colourName)
+            SetLedBorg(red, green, blue)
+    except Exception as e:                                             # If we have an error
+        print("ZESER:")
+        print(e)
+        pass                                                # Ignore it (do nothing)
+    finally:                                            # Regardless of errors:
+        time.sleep(10)
