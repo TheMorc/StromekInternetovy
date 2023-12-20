3ed45b23e4c1ac2cb93da4a8784451ac19abed4c
TheMorc <r.gracik@gmail.com>
2023-12-20 20:37:54 +0100

    mastodon kamera tooter

---
+++ stromekToot.py
@@ -0,0 +1,20 @@
+from picamera import PiCamera
+from time import sleep
+from datetime import datetime
+from gpiozero import CPUTemperature
+from mastodon import Mastodon#
+
+mastodon = Mastodon(api_base_url = "https://mastodon.social", access_token = "")
+curr_time = datetime.now()
+cpu = CPUTemperature()
+cam_toot_text = curr_time.strftime("%d.%m.%Y %H:%M") + " | RPi 1 A+ " + str(round(cpu.temperature,1)) + "'C"
+
+camera = PiCamera()
+camera.resolution = (1296, 972)
+camera.framerate = 1
+camera.annotate_text = cam_toot_text
+camera.capture('/home/pi/StromekInternetovy/snapshot.jpg')
+sleep(5)
+snapshot = mastodon.media_post("/home/pi/StromekInternetovy/snapshot.jpg", mime_type="image/jpeg")
+mastodon.status_post("Biskupovský stromek\n"  + cam_toot_text, media_ids=[snapshot.id])
+#print(snapshot)
