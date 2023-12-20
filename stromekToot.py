2a262dbaed2dbf94d4e44040e1d80eae4c9d4b37
TheMorc <r.gracik@gmail.com>
2023-12-21 00:17:47 +0100

    netootuvať v noci, tedy stromek nesvíti

--- stromekToot.py
+++ stromekToot.py
@@ -9,12 +9,15 @@ curr_time = datetime.now()
 cpu = CPUTemperature()
 cam_toot_text = curr_time.strftime("%d.%m.%Y %H:%M") + " | RPi 1 A+ " + str(round(cpu.temperature,1)) + "'C"
 
-camera = PiCamera()
-camera.resolution = (1296, 972)
-camera.framerate = 1
-camera.annotate_text = cam_toot_text
-camera.capture('/home/pi/StromekInternetovy/snapshot.jpg')
-sleep(5)
-snapshot = mastodon.media_post("/home/pi/StromekInternetovy/snapshot.jpg", mime_type="image/jpeg")
-mastodon.status_post("Biskupovský stromek\n"  + cam_toot_text, media_ids=[snapshot.id])
-#print(snapshot)
+currHour = int(curr_time.strftime("%H"))
+if currHour <= 23 and currHour >= 6 or currHour == 0:
+	print("preparing to toot")
+	camera = PiCamera()
+	camera.resolution = (1296, 972)
+	camera.framerate = 1
+	camera.annotate_text = cam_toot_text
+	camera.capture('/home/pi/StromekInternetovy/snapshot.jpg')
+	sleep(5)
+	snapshot = mastodon.media_post("/home/pi/StromekInternetovy/snapshot.jpg", mime_type="image/jpeg")
+	mastodon.status_post("Biskupovský stromek\n"  + cam_toot_text, media_ids=[snapshot.id])
+	#print(snapshot)
