a9deb2c118f782e0cd70164ac9af470c3d0b374b
TheMorc <r.gracik@gmail.com>
2023-12-21 00:16:52 +0100

    compileUpload.sh: zas iný port

--- compileUpload.sh
+++ compileUpload.sh
@@ -1,2 +1,2 @@
 arduino-cli compile --fqbn arduino:avr:uno UnoStromek
-arduino-cli upload -p /dev/cu.usbmodem2101 --fqbn arduino:avr:uno UnoStromek
+arduino-cli upload -p /dev/cu.usbmodem11301 --fqbn arduino:avr:uno UnoStromek
