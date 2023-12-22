f80868e12170411bd446d31709ac97a3b4344592
TheMorc <r.gracik@gmail.com>
2023-12-22 14:12:01 +0100

    arduino hotfix, nebanguvaní API a sústavné neposílaní

--- cheerlights.py
+++ cheerlights.py
@@ -16,7 +16,8 @@ import colorsys
 from datetime import datetime
 
 arduino = serial.Serial("/dev/ttyACM0", 115200)
-arduino.read()
+print("Arduino reset delay")
+time.sleep(3)
 
 # A function to set the LedBorg colours
 def SetLedBorg(red, green, blue):
@@ -43,11 +44,12 @@ colourMap = {'red':         (1.0,   0.0,    0.0),
              'pink':        (1.0,   0.75,   0.8),
              'oldlace':     (1.0,   1.0,    0.9)}
 
+lastColor = ""
 lastHour = -2147
 currentHour = -2137
 
 while True:
-    print("current hour:" + str(currentHour) + " | lastHour: " + str(lastHour))
+    #print("lastColor:" + str(lastColor))
     now = datetime.now()
     currentHour = int(now.strftime("%H"))
     if currentHour != lastHour:
@@ -55,27 +57,30 @@ while True:
         if currentHour == 6:
             print("full brightness")
             arduino.write("BR1".encode())
-        elif currentHour == 21:
+        elif currentHour >= 19 and currentHour <= 21:
             print("half brightness")
             arduino.write("BR2".encode())
-        elif currentHour == 0:
+        elif currentHour == 22 or currentHour == 23 or currentHour == 0:
             print("low brightness")
             arduino.write("BR1".encode())
         elif currentHour == 1:
             print("no brightness")
             arduino.write("BR0".encode())
     if (arduino.inWaiting() > 0):
-        data_str = arduino.read(arduino.inWaiting()).decode('utf-8') 
+        data_str = arduino.read(arduino.inWaiting()).decode('utf-8')
         print(data_str)
-    try:                                                # Attempt the following:
-        with urlopen(cheerlightsUrl) as colourResponse:
-            colourName = colourResponse.read().decode("utf-8")
-            if colourName in colourMap:                   # If we recognise this colour name then ...
-                red, green, blue = colourMap[colourName]            # Get the LedBorg colour to use from the name
-            else:
-                red, green, blue = (0.0, 0.5, 0.5)
-            print("current web color: " + colourName)
-            SetLedBorg(red, green, blue)
+    try:
+        if currentHour <= 23 and currentHour >= 6 or currentHour == 0:
+            with urlopen(cheerlightsUrl) as colourResponse:
+                colourName = colourResponse.read().decode("utf-8")
+                if colourName in colourMap:                   # If we recognise this colour name then ...
+                    red, green, blue = colourMap[colourName]            # Get the LedBorg colour to use from the name
+                else:
+                    red, green, blue = (0.0, 0.5, 0.5)
+                if lastColor != colourName:
+                    SetLedBorg(red, green, blue)
+                    print("new web color: " + colourName)
+                lastColor = colourName
     except Exception as e:                                             # If we have an error
         print("ZESER:")
         print(e)
